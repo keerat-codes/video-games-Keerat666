c77b72997fc4828199f91c0cfe34e3ee2d12616a
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-29 20:13:19 +0530

    Updated App.js for gamesModel route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const gamesModelRoute = require('./routes/gamesModel');
+app.use('/gamesModel', gamesModelRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
